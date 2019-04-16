9707f6c9a509d6b151634fb47e25ead9407e0d3c
Darius Jankauskas <d5926j@gmail.com>
2019-04-16 12:31:02 -0400

    Implemented in-place selection sort.

--- OrderedList_inArraySlots.java
+++ OrderedList_inArraySlots.java
@@ -1,3 +1,4 @@
+
 /**
   OrderedList_inArraySlots, holding Integers
 
@@ -5,168 +6,160 @@
   increasing value, implemented using a
   ArrayList<Integer>
  */
-import java.util.ArrayList;  // shortcut
+import java.util.ArrayList; // shortcut
 
-public class OrderedList_inArraySlots
-    implements OrderedList {
+public class OrderedList_inArraySlots implements OrderedList {
 
-    private ArrayList<Integer> list_iAS;
+  private ArrayList<Integer> list_iAS;
 
-    /** 
-      construct order from an unordered ArrayList
-     */
-    public OrderedList_inArraySlots
-            ( ArrayList<Integer> unordered) {
-        this();  // violates the directions for this hw
-        
-        System.out.println( 
-            "Change this to report on progress."
-          + System.lineSeparator()
-          + "You were going to do that even without prompting, right?"
-          );
+  /**
+   * construct order from an unordered ArrayList. Elements may not be null.
+   */
+  public OrderedList_inArraySlots(ArrayList<Integer> unordered) {
+    list_iAS = unordered;
+    for (int i = 0; i < list_iAS.size(); i++) {
+      int champIndex = champIndex(i, list_iAS.size());
+      swap(i, champIndex);
     }
+  }
 
-
-    /** 
-      helper function for constructor
-      Write good English here, reflecting good thinking.
-      @return ??
-     */
-     private int champIndex() {
-        return 0;  // replace this line
-     }
-
-
-    // ------ code from previous assignments below here ----
-
-    /**
-      @return the index of any occurrence of
-              \findMe in this list, or -1 if
-              \findMe is absent from this list.
-     */
-    public int indexOf( Integer findMe) {
-        return indexOf_whileStyle( findMe);
-        // return indexOf_recursive(
-            // findMe, 0, list_iAS.size() -1);
+  /**
+   * Takes a range over list_iAS and finds the smallest element in the range [startIndex, endIndex)
+   * 
+   * @return the index of smallest element in the range
+   */
+  private int champIndex(int startIndex, int endIndex) {
+    Integer smallest = list_iAS.get(startIndex);
+    int smallestIndex = 0;
+    int i = startIndex;
+    for(; i < list_iAS.size(); i++) {
+      if (list_iAS.get(i).compareTo(smallest) < 1) {
+        smallest = list_iAS.get(i);
+        smallestIndex = i;
+      }
     }
+    return smallestIndex;
+  }
 
+  private void swap(int index, int secondIndex) {
+    Integer temp = list_iAS.get(index);
+    list_iAS.set(index, list_iAS.get(secondIndex));
+    list_iAS.set(secondIndex, temp);
+  }
 
-    /**
-      @return the indexOf value, calculated while-style
-     */
-    private int indexOf_whileStyle( Integer findMe) {
-        int low = 0;
-        int hi  = list_iAS.size() -1;  // inclusive
+  // ------ code from previous assignments below here ----
 
-        while( low <= hi){
-            int pageToCheck = (low + hi) / 2;
-            int comparison =
-              findMe.compareTo( list_iAS.get( pageToCheck));
-            if( comparison == 0) return pageToCheck;
-            else
-                if( comparison < 0)
-                    // findMe's spot precedes pageToCheck
-                    hi = pageToCheck -1;
-                // findMe's spot follows pageToCheck
-                else low = pageToCheck +1;
-        }
-        return -3; // value differs from skeleton, just FYI
+  /**
+   * @return the index of any occurrence of \findMe in this list, or -1 if \findMe
+   *         is absent from this list.
+   */
+  public int indexOf(Integer findMe) {
+    return indexOf_whileStyle(findMe);
+    // return indexOf_recursive(
+    // findMe, 0, list_iAS.size() -1);
+  }
+
+  /**
+   * @return the indexOf value, calculated while-style
+   */
+  private int indexOf_whileStyle(Integer findMe) {
+    int low = 0;
+    int hi = list_iAS.size() - 1; // inclusive
+
+    while (low <= hi) {
+      int pageToCheck = (low + hi) / 2;
+      int comparison = findMe.compareTo(list_iAS.get(pageToCheck));
+      if (comparison == 0)
+        return pageToCheck;
+      else if (comparison < 0)
+        // findMe's spot precedes pageToCheck
+        hi = pageToCheck - 1;
+      // findMe's spot follows pageToCheck
+      else
+        low = pageToCheck + 1;
     }
+    return -3; // value differs from skeleton, just FYI
+  }
 
+  /**
+   * @return the indexOf value, calculated recursively [Holmes's comments
+   *         temporarily elided, so as to avoid spoilers for hw60_16]
+   */
+  private int indexOf_recursive(Integer findMe, int low, int hi // inclusive
+  ) {
+    // System.out.println( "debug low: " + low
+    // + " hi: " + hi);
+    if (low > hi) // detect base case
+      return -2; // solution to base case
+    // value differs from while-style method, just FYI
+    else {
+      int pageToCheck = (low + hi) / 2;
+      int comparison = findMe.compareTo(list_iAS.get(pageToCheck));
 
-    /**
-      @return the indexOf value, calculated recursively
-      [Holmes's comments temporarily elided, so as
-       to avoid spoilers for hw60_16]
-     */
-    private int indexOf_recursive( Integer findMe
-                                 , int low
-                                 , int hi // inclusive
-                                 ) {
-        // System.out.println( "debug low: " + low
-                          // + "   hi: " + hi);
-        if( low > hi)  // detect base case
-            return -2;   // solution to base case
-              // value differs from while-style method, just FYI
-        else{
-            int pageToCheck = (low + hi) / 2;
-            int comparison =
-              findMe.compareTo( list_iAS.get( pageToCheck));
-
-
-            if( comparison == 0)    // detect base case
-                return pageToCheck; // solution other base case
-            // recursive cases
-            else
-                if( comparison < 0)
-                    // findMe's spot precedes pageToCheck
-                    return indexOf_recursive( findMe
-                                             , low
-                                             , pageToCheck -1);
-                else
-                    // findMe's spot follows pageToCheck
-                    return indexOf_recursive( findMe
-                                            , pageToCheck +1
-                                            , hi);
-        }
+      if (comparison == 0) // detect base case
+        return pageToCheck; // solution other base case
+      // recursive cases
+      else if (comparison < 0)
+        // findMe's spot precedes pageToCheck
+        return indexOf_recursive(findMe, low, pageToCheck - 1);
+      else
+        // findMe's spot follows pageToCheck
+        return indexOf_recursive(findMe, pageToCheck + 1, hi);
     }
+  }
 
+  public OrderedList_inArraySlots() {
+    list_iAS = new java.util.ArrayList<Integer>();
+  }
 
-    public OrderedList_inArraySlots() {
-        list_iAS = new java.util.ArrayList<Integer>();
-    }
+  /**
+   * @return the number of elements in this list
+   */
+  public int size() {
+    return list_iAS.size();
+  }
 
-    /**
-      @return the number of elements in this list
-     */
-    public int size(){
-        return list_iAS.size();
-    }
+  /**
+   * @return a string representation of this Orderedlist_iAS
+   */
+  public String toString() {
+    return list_iAS.toString();
+  }
 
-    /**
-      @return a string representation of this Orderedlist_iAS
-     */
-    public String toString() {
-        return list_iAS.toString();
-    }
+  /**
+   * Put @value where it belongs in the list.
+   * 
+   * @pre: the list is in increasing order
+   */
+  public boolean add(Integer value) {
+    int dest = 0;
+    for (; dest < list_iAS.size() && list_iAS.get(dest) < value; dest++)
+      ;
+    // System.out.println( "OL adding " + value
+    // + " at index " + dest);
+    list_iAS.add(dest, value);
+    return true;
+  }
 
+  /**
+   * @return element @index from this list precondition: @index is within the
+   *         bounds of the array. (Having warned the user about this precondition,
+   *         you should NOT complicate your code to check whether the condition
+   *         was violated.)
+   */
+  public Integer get(int index) {
+    return list_iAS.get(index);
+  }
 
-    /**
-      Put @value where it belongs in the list.
-      @pre: the list is in increasing order
-     */
-     public boolean add( Integer value) {
-         int dest = 0;
-         for( ; dest < list_iAS.size() && list_iAS.get( dest) < value
-              ; dest++) ;
-         // System.out.println( "OL adding " + value
-         //                   + " at index " + dest);
-         list_iAS.add( dest, value);
-         return true;
-     }
-
-
-     /**
-      @return element @index from this list
-      precondition: @index is within the bounds of the array.
-          (Having warned the user about this precondition,
-           you should NOT complicate your code to check
-           whether the condition was violated.)
-     */
-    public Integer get( int index ) {
-        return list_iAS.get( index);
-    }
-
-
-    /**
-      Remove the element at position @index in this list.
-
-      Shift any subsequent elements to the left (that is,
-      decrease the index associated with each).
-
-      @return the value that was removed from the list
-     */
-    public Integer remove( int index) {
-        return list_iAS.remove( index);
-    }
+  /**
+   * Remove the element at position @index in this list.
+   * 
+   * Shift any subsequent elements to the left (that is, decrease the index
+   * associated with each).
+   * 
+   * @return the value that was removed from the list
+   */
+  public Integer remove(int index) {
+    return list_iAS.remove(index);
+  }
 }
